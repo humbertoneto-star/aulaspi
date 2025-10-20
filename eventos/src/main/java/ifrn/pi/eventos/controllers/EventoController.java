c4af04d35c818ecb9f3ea568ca6038adc6d519bd
humbertoneto-star <humberto.neto@escolar.ifrn.edu.br>
2025-10-20 15:29:24 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ eventos/src/main/java/ifrn/pi/eventos/controllers/EventoController.java
@@ -0,0 +1,31 @@
+package ifrn.pi.eventos.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.EventoRepository;
+
+@Controller
+public class EventoController {
+	
+	@Autowired
+	private EventoRepository er;
+	
+	@RequestMapping("/eventos/form")
+	public String form() {
+		return "eventos/formEvento";
+	}
+	
+	@PostMapping("/eventos")
+	public String adicionar(Evento evento) {
+		
+		System.out.println(evento);
+		er.save(evento);
+		
+        return "eventos/evento-adicionado";
+	}
+
+}
